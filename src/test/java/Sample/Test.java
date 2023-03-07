5608e5a9a04e34cac16fb46701a164441ccac04c
vjanhavi28 <126957050+vjanhavi28@users.noreply.github.com>
2023-03-07 23:10:15 +0530

    Update Test.java

--- src/test/java/Sample/Test.java
+++ src/test/java/Sample/Test.java
@@ -5,8 +5,10 @@ public class Test {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
-		//pushed done in git
-		//pulled done in eclipse
+		//pushed done in git(now pull to eclipse)
+		
+		//pulled done in eclipse(push to git)
+		//2nd pushed done in git(now pull to eclipse)
 
 	}
 
